cc08eefe8269e24ef5829c818a2b98f6fd36f2d8
Carlos Osorio <gimcode6@gmail.com>
2026-02-13 16:06:14 -0600

    Uploaded firmware

---
+++ firmware/servo.ino
@@ -0,0 +1,23 @@
+
+#include <Servo.h>
+
+Servo servoMotor;
+
+void setup() {
+  Serial.begin(9600);
+  servoMotor.attach(9);
+}
+
+void loop() {
+  servoMotor.write(0);
+
+  delay(1000);
+
+  servoMotor.write(90);
+
+  delay(1000);
+  
+  servoMotor.write(180);
+
+  delay(1000);
+}
