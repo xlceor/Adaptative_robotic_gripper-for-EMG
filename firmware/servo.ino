c8cecbdb0bf5bd7288d4caf20a9ffea756abbe6b
Carlos Osorio <gimcode6@gmail.com>
2026-04-07 23:41:35 -0600

    Update firmware to add wireless control

--- firmware/servo.ino
+++ firmware/servo.ino
@@ -1,23 +1,58 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESP32Servo.h>
 
-#include <Servo.h>
+const char* ssid = "";
+const char* password = "";
 
-Servo servoMotor;
+WebServer server(80);
+
+Servo servoA;
+Servo servoB;
+
+int pinServoA = 15;
+int pinServoB = 16;
+
+void moverON() {
+    servoA.write(180);
+    servoB.write(0);
+    server.send(200, "text/plain", "Mode ON: A=180, B=0");
+}
+
+void moverOFF() {
+    servoA.write(0);
+    servoB.write(180);
+    server.send(200, "text/plain", "Mode OFF: A=0, B=180");
+}
 
 void setup() {
-  Serial.begin(9600);
-  servoMotor.attach(9);
+    Serial.begin(115200);
+
+    servoA.attach(pinServoA, 500, 2400);
+    servoB.attach(pinServoB, 500, 2400);
+
+    servoA.write(0);
+    servoB.write(180);
+
+
+    WiFi.begin(ssid, password);
+    Serial.print("Conecting");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nConected!");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/on", moverON);
+    server.on("/off", moverOFF);
+
+    server.begin();
 }
 
 void loop() {
-  servoMotor.write(0);
-
-  delay(1000);
-
-  servoMotor.write(90);
-
-  delay(1000);
-  
-  servoMotor.write(180);
-
-  delay(1000);
+    server.handleClient();
 }
