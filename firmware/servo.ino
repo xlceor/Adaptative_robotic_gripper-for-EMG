e3f880ff09f51f91a3a53595aae5cfa746200e7b
Carlos Osorio <gimcode6@gmail.com>
2026-04-08 13:10:00 -0600

    Added websockets to fluid updating

--- firmware/servo.ino
+++ firmware/servo.ino
@@ -1,58 +1,97 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
 const char* ssid = "";
 const char* password = "";
 
-WebServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-Servo servoA;
-Servo servoB;
+Servo servo1;
+Servo servo2;
 
-int pinServoA = 15;
-int pinServoB = 16;
+#define SERVO1_PIN 15
+#define SERVO2_PIN 16
 
-void moverON() {
-    servoA.write(180);
-    servoB.write(0);
-    server.send(200, "text/plain", "Mode ON: A=180, B=0");
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_DATA) {
+    JsonDocument doc; 
+    DeserializationError error = deserializeJson(doc, data, len);
+
+    if (error) return;
+
+  if (doc.containsKey("servo")) {
+    int valorRecibido = doc["servo"].as<int>(); 
+    
+    int angle = constrain(valorRecibido, 0, 180);
+    
+    servo1.write(angle);
+    servo2.write(180 - angle);
+  }
+  }
 }
 
-void moverOFF() {
-    servoA.write(0);
-    servoB.write(180);
-    server.send(200, "text/plain", "Mode OFF: A=0, B=180");
+float fakeTemp() {
+  static float value = 20.0;
+  static bool up = true;
+  value += (up ? 0.2 : -0.2);
+  if (value >= 30.0) up = false;
+  if (value <= 20.0) up = true;
+  return value;
+}
+
+int fakeADC() {
+  static int value = 0;
+  static bool up = true;
+  value += (up ? 20 : -20);
+  if (value >= 4095) up = false;
+  if (value <= 0) up = true;
+  return value;
 }
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    servoA.attach(pinServoA, 500, 2400);
-    servoB.attach(pinServoB, 500, 2400);
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  
+  servo1.setPeriodHertz(50); 
+  servo2.setPeriodHertz(50);
+  
+  servo1.attach(SERVO1_PIN, 500, 2400); 
+  servo2.attach(SERVO2_PIN, 500, 2400);
 
-    servoA.write(0);
-    servoB.write(180);
-
-
-    WiFi.begin(ssid, password);
-    Serial.print("Conecting");
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
     Serial.println("\nConected!");
     Serial.print("IP: ");
     Serial.println(WiFi.localIP());
 
-    server.on("/on", moverON);
-    server.on("/off", moverOFF);
-
-    server.begin();
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+  server.begin();
 }
 
 void loop() {
-    server.handleClient();
+  ws.cleanupClients();
+
+  static unsigned long last = 0;
+  if (millis() - last > 1000) {
+    last = millis();
+
+    JsonDocument doc;
+    doc["temp"] = fakeTemp();
+    doc["adc"] = fakeADC();
+
+    String output;
+    serializeJson(doc, output);
+    ws.textAll(output);
+  }
 }
